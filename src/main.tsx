f5b812bb9fd0d307739837f51b3442dea93985d6
Andrew Hale <andyhalebmx@hotmail.com>
2024-01-29 18:54:35 +0000

    minor css and react router changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import "./index.css";
+// import "./index.css";
+import "./_main.scss";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
